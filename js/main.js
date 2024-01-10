c79421344e2326ad0bd0dc048579f67acf37793f
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2024-01-10 15:48:21 +0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
-const btn = document.querySelector(".basket__btn");
+let count = 0;
+const btn = document.querySelectorAll(".basket__btn");
 const span = document.querySelector(".plus");
-btn.addEventListener("click", () => {
-  span.innerHTML++;
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", () => {
+    count++;
+    span.innerHTML = count;
+  });
+}
+
+let count2 = 0;
+const btn2 = document.querySelectorAll(".basket__btn");
+const span2 = document.querySelector(".plus2");
+
+for (let i = 0; i < btn2.length; i++) {
+  btn2[i].addEventListener("click", () => {
+    count2++;
+    span2.innerHTML = count2;
+  });
+}
+
+let toggle = document.querySelector(".nav__toggle");
+let menu = document.querySelector(".katalog");
+let main = document.getElementById("main");
+let isOpen = false;
+
+toggle.addEventListener("click", () => {
+  isOpen = !isOpen;
+  if (isOpen) {
+    menu.style.opacity = "1";
+  } else {
+    menu.style.opacity = "0";
+  }
 });
